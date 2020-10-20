d449d30d983428ce14a8596e918897aedabd1f0d
Segey <vorobyishkaa@mail.ru>
2020-10-20 19:40:03 +0300

    added button to circle

--- fishkey.js
+++ fishkey.js
@@ -27,3 +27,61 @@ function vectorDraw_init(vd_SelectorsForSVG, vd_svgs) {
 function vectorDraw(logoPaths, vector) {
     logoPaths[vector].classList.add('draw-svg');
 }
+
+/* кнопка вжух в кружок */
+function buttonToCircle_init(selector, minWidth) {
+    const buttonToCircle = $(selector),
+        buttonTextHolder = $(buttonToCircle).children(),
+        buttonStyle = {
+            'bgColor': $(buttonTextHolder).css('background-color'),
+            'borderRadius': getComputedStyle(buttonTextHolder[0]).borderStartEndRadius,
+            'width': buttonTextHolder[0].offsetWidth,
+            'height': buttonTextHolder[0].offsetHeight
+        };
+
+    if ($(window).width() > minWidth) {
+        const widthShift = 2*parseInt(buttonStyle.borderRadius, 10);
+        $(buttonToCircle).prepend(`<div class='moving_bg'></div>`);
+        const movingBg = $('.moving_bg');
+        $(buttonTextHolder).html(`<p class='buttonToCircleTxt'>${$(buttonTextHolder).text()}</p>`);
+
+        $(buttonTextHolder).css({
+            'position': 'relative',
+            'background': 'transparent',
+            'transition': '0.7s cubic-bezier(0.9, 0, 0.1, 1)',
+            'z-index': 100,
+            'border': '1px solid ' + buttonStyle.bgColor
+        });
+        $(buttonToCircle).css({
+            'overflow': 'hidden',
+            'width': buttonStyle.width,
+            'height': buttonStyle.height
+        });
+        $(movingBg).css({
+            'width': buttonStyle.width,
+            'height': buttonStyle.height,
+            'position': 'absolute',
+            'z-index': 99,
+            'background': buttonStyle.bgColor,
+            'border-radius': buttonStyle.borderRadius,
+            'transition': '0.7s cubic-bezier(0.9, 0, 0.1, 1)',
+            'right': '0'
+        });
+        $('.buttonToCircleTxt').css({
+            'position': 'relative',
+            'transition': '0.7s cubic-bezier(0.9, 0, 0.1, 1)',
+            'left': '0'
+        });
+
+        $(buttonToCircle).hover(function() {
+            $(movingBg).css('width', widthShift);
+            $(buttonTextHolder).css('color', buttonStyle.bgColor);
+            $('.buttonToCircleTxt').css('left', `-${widthShift/3}px`);
+        },
+        function() {
+            $(movingBg).css('width', buttonStyle.width);
+            $(buttonTextHolder).css('color', 'white');
+            $('.buttonToCircleTxt').css('left', `0`);
+        });
+    }
+}
