d15e4241bad6fe650ed917f95f869f8befb2ed23
Segey <vorobyishkaa@mail.ru>
2021-01-23 12:28:01 +0300

    fixed cornerPhotos

--- fishkey.js
+++ fishkey.js
@@ -1785,27 +1785,69 @@ function cursorTail_init(params) {
 function cornerPhotos_init(params) {
     const cornerPhotos = document.querySelectorAll(params.photos),
         initialVisibility = params.initialVisibility || 0,
+        startPos = params.startPos || 'left-top',
         transitionTime = params.transitionTime || 0.5,
+        isHorScroll = params.isHorScroll || false,
         minWidth = params.minWidth || 0,
         offsets = [];
-
-    cornerPhotos.forEach((photo, i) => {
-        const offsetPercentage = params.offsets[i] || 0;
-        offsets[i] = offsetPercentage*$(window).height()/100;
-    });
+    let showingRule;
+    const ww = $(window).width(),
+        wh = $(window).height();
 
     if ($(window).width() > minWidth) {
+        let clipPath = '';
+        switch (startPos) {
+            case 'left-top':
+                clipPath = `polygon(0 0, ${initialVisibility}% 0, ${initialVisibility}% ${initialVisibility}%, 0 ${initialVisibility}%)`;
+                break;
+            case 'top':
+                clipPath = `polygon(0 0, 100% 0, 100% ${initialVisibility}%, 0 ${initialVisibility}%)`;
+                break;
+            case 'right-top':
+                clipPath = `polygon(${100 - initialVisibility}% 0, 100% 0, 100% ${initialVisibility}%, ${100 - initialVisibility}% ${initialVisibility}%)`;
+                break;
+            case 'right':
+                clipPath = `polygon(${100 - initialVisibility}% 0, 100% 0, 100% 100%, ${100 - initialVisibility}% 100%)`;
+                break;
+            case 'right-bottom':
+                clipPath = `polygon(${100 - initialVisibility}% ${100 - initialVisibility}%, 100% ${100 - initialVisibility}%, 100% 100%, ${100 - initialVisibility}% 100%)`;
+                break;
+            case 'bottom':
+                clipPath = `polygon(0 ${100 - initialVisibility}%, 100% ${100 - initialVisibility}%, 100% 100%, 0 100%)`;
+                break;
+            case 'left-bottom':
+                clipPath = `polygon(0 ${100 - initialVisibility}%, ${initialVisibility}% ${100 - initialVisibility}%, ${initialVisibility}% 100%, 0 100%)`;
+                break;
+            case 'left':
+                clipPath = `polygon(0 0, ${initialVisibility}% 0, ${initialVisibility}% 100%, 0 100%)`;
+                break;
+            default:
+                clipPath = `polygon(0 0, ${initialVisibility}% 0, ${initialVisibility}% ${initialVisibility}%, 0 ${initialVisibility}%)`;
+                break;
+        }
+
         cornerPhotos.forEach((photo, i) => {
-            $(photo).append(`
-                <svg style="width: 100%; height: 100%; position: absolute; left: 0;">
-                    <defs>
-                        <clipPath id="mask-${i}" style="transform: scale(${initialVisibility}); transition: transform ${transitionTime}s ease">
-                            <rect width="100%" height="100%" fill="#FFFFFF"></rect>
-                        </clipPath>
-                    </defs>
-                </svg>
-            `);
-            photo.style.clipPath = `url(#mask-${i})`;
+            const offsetPercentage = params.offsets[i] || 0;
+            if (isHorScroll) {
+                offsets[i] = offsetPercentage*$(window).width()/100;
+                showingRule = function (photo, i) {
+                    const et = $(photo).offset().left;
+                    return (ww - offsets[i] > et)
+                };
+            } else {
+                offsets[i] = offsetPercentage*$(window).height()/100;
+                showingRule = function (photo, i) {
+                    const et = $(photo).offset().top;
+                    return ($(window).scrollTop() + wh - offsets[i] > et)
+                };
+            }
+
+            $(photo).css({
+                'clip-path': clipPath,
+                transition: `${transitionTime}s ease`
+            });
+
+            photo.setAttribute('data-clipping', i);
             photo.setAttribute('data-clipped', 'true');
         });
     
@@ -1813,13 +1855,9 @@ function cornerPhotos_init(params) {
     }
     
     function showOnScroll() {
-        const wt = $(window).scrollTop(),
-            wh = $(window).height();
-
         cornerPhotos.forEach((photo, i) => {
-            const et = $(photo).offset().top;
-            if((wt + wh - offsets[i] > et) && (photo.getAttribute('data-clipped') == 'true')) {
-                document.querySelector(`#mask-${i}`).style.transform = 'scale(1)';
+            if(showingRule(photo, i) && (photo.getAttribute('data-clipped') == 'true')) {
+                $(photo).css('clip-path', 'polygon(0 0, 100% 0, 100% 100%, 0 100%)');
                 photo.setAttribute('data-clipped', 'false');
             }
         });
