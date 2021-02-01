759e3879e81aea4d5b02b1483aa52b17097913f6
Segey <vorobyishkaa@mail.ru>
2021-02-01 18:50:46 +0300

    added getElemData

--- fishkey.js
+++ fishkey.js
@@ -1,3 +1,30 @@
+/* утилита для получения значений размеров на разной ширине экрана */
+function getElemDim (elem, dim) {
+    const tildaBreakpoints = [1200, 980, 640, 480, 320];
+    const ww = $(window).width();
+    let currentBreakpoint;
+    let result = null;
+    const queries = [
+        `data-field-${dim}-value`,
+        `data-field-${dim}-res-960-value`,
+        `data-field-${dim}-res-640-value`,
+        `data-field-${dim}-res-480-value`,
+        `data-field-${dim}-res-320-value`
+    ];
+    for(let i = 0; i < tildaBreakpoints.length; i++) {
+        if (ww > tildaBreakpoints[i]) {
+            currentBreakpoint = i;
+            break;
+        }
+    }
+    for(let i = currentBreakpoint; i >= 0; i--) {
+        result = elem.getAttribute(queries[i]);
+        if (result) {
+            return result;
+        }
+    }
+}
+
 /* утилита для плавного расчета координат */
 function initCoordTracking(obj, trigger, positioning, hasX, hasY, params) {
     let isIntSet = false;
