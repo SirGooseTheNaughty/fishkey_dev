b0ffdd1de66d775f9da822561a3e95720856107b
Segey <vorobyishkaa@mail.ru>
2021-02-10 16:53:40 +0300

    added index setting to bgnoise

--- fishkey.js
+++ fishkey.js
@@ -324,9 +324,11 @@ function bgNoise_init(parameters) {
     const bgNoiseBlock = document.querySelector(parameters.selector),
         bgNoiser = bgNoiseBlock.querySelector('[data-elem-type="shape"]'),
         bgGrainer = bgNoiser.querySelector('.tn-atom'),
-        grain = parameters.grain ? parameters.grain + 'px' : 'auto';
+        grain = parameters.grain ? parameters.grain + 'px' : 'auto',
+        zIndex = parameters.isCovering ? 999999999 : 0;
     bgNoiser.classList.add('bg-noise');
     bgNoiser.style.opacity = parameters.opacity/100 || 0.05;
+    bgNoiser.style.zIndex = zIndex;
     bgNoiseBlock.style.height = '0';
     bgNoiseBlock.style.overflow = 'hidden';
     bgGrainer.style.backgroundRepeat = 'repeat';
