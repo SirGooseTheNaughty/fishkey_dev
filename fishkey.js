7a12c7f9ada21ec9a3176b92a54fa1a398067f32
Sergey Vorobyev <syvorobyev@gmail.com>
2021-06-02 14:00:01 +0300

    hoverText fixed

--- fishkey.js
+++ fishkey.js
@@ -1307,7 +1307,6 @@ function hoverText_init(params) {
         });
     
         $(".textHover").mouseenter(function(event) {
-            console.log(this.getAttribute('data-text'));
                 hoverTextCursor.firstElementChild.innerText = this.getAttribute('data-text');
                 hoverTextCursor.style.transition = 'opacity 0.25s ease';
                 hoverTextCursor.style.opacity = '1';
