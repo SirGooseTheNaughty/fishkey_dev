0e609e99e5c5644b5915471c03cc35fb8504cdea
Sergey Vorobyev <syvorobyev@gmail.com>
2021-10-11 10:28:58 +0300

    fix typewriter

--- fishkey.js
+++ fishkey.js
@@ -1291,26 +1291,24 @@ function textApp_init(parameters) {
 function typeWriter_init(parameters) {
     const totalSpeed = parameters.totalSpeed || 2000;
     const minWidth = parameters.minWidth || 0;
-    let offset = parameters.offset || 0;
-    const tw_TextElem = document.querySelector(parameters.selector).firstElementChild,
-        tw_Text = tw_TextElem.innerText.split("");
+    const offset = parameters.offset ? $(window).height()*parameters.offset/100 : 0;
+    const tw_TextElem = document.querySelector(parameters.selector).firstElementChild;
+    const tw_Text = tw_TextElem.innerText.split("");
 
     if ($(window).width() > minWidth) {
         tw_TextElem.innerText = '';
+
+        document.addEventListener('DOMContentLoaded', () => {
+            tw_startWriting();
+            document.addEventListener('scroll', tw_startWriting);
+        });
     }
-
-    offset = $(window).height()*offset/100;
-
-    document.addEventListener('DOMContentLoaded', () => {
-        tw_startWriting();
-        document.addEventListener('scroll', tw_startWriting);
-    })
 
     function tw_write() {
         const speed = totalSpeed / tw_Text.length;
         const tw_interval = setInterval(function() {
             if(!tw_Text[0]){
-                    return clearInterval(tw_interval);
+                return clearInterval(tw_interval);
             }
             tw_TextElem.innerHTML += tw_Text.shift();
         }, speed);
