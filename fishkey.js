c5a363901d4b297b86548b9c5c5b421a3fbb2c2f
Sergey Vorobyev <syvorobyev@gmail.com>
2021-11-24 16:49:21 +0300

    added global counter

--- fishkey.js
+++ fishkey.js
@@ -1,4 +1,5 @@
 const tildaBreakpoints = [1200, 980, 640, 480, 320];
+let globalCounter = 0;
 
 /* утилита для определения браузера */
 /* Названия браузеров: chrome, firefox, safari, explorer, edge, opera, samsung */
